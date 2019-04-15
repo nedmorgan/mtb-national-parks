30a6d98c8682c8f84a22ad927979697d57a7bf6d
nedmorgan <nmorgan1987@gmail.com>
2019-04-15 14:17:34 -0400

    created views

--- mtb_trails_app/views.py
+++ mtb_trails_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import StateSerializer, ParkSerializer, TrailSerializer, BikeSerializer
+from .models import State, Park, Trail, Bike
+
+
+class StateView(viewsets.ModelViewSet):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+
+class ParkView(viewsets.ModelViewSet):
+    queryset = Park.objects.all()
+    serializer_class = ParkSerializer
+
+
+class TrailView(viewsets.ModelViewSet):
+    queryset = Trail.objects.all()
+    serializer_class = TrailSerializer
+
+
+class BikeView(viewsets.ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
