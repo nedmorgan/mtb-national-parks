5a36f5a59eaabeb9bbb9ea5bc08b920adf20d1fc
nedmorgan <nmorgan1987@gmail.com>
2019-04-16 11:03:56 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import 'bulma/css/bulma.css'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <NavBar />
           <Switch>
             {/* <Route exact path='/' component={ArtistList} />
             <Route path='/artists/:id' component={Artist} /> */}
---
+++ client/src/components/styled_components/NavBarStyles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.div`
+
+.navbar {
+    font-family: "National Park Regular", sans-serif;
+    background: rgba(200,200,200,.5) !important;
+}
+`
